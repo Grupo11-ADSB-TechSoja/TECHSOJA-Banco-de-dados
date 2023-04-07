6277fc81ff26d07360a584da0de0a00257f4d285
arthur06martins <arthur12martins12@gmail.com>
2023-04-07 13:13:06 -0300

    inserindo dados

--- scriptTechSojaBD.sql
+++ scriptTechSojaBD.sql
@@ -10,7 +10,7 @@ Senha varchar (45)not null,
 Email varchar (80)not null,
 TelefoneCel char(9)not null
 );
-
+ 
 create table Estabelecimento (
 idEstabelecimento int primary key auto_increment,
 CNPJ char(10) not null,
@@ -39,3 +39,34 @@ Bairro varchar(45) not null,
 Cidade varchar(45) not null,
 fkEstabelecimento int, foreign key (fkEstabelecimento) references Estabelecimento (idEstabelecimento)
 );
+
+alter table Representante modify column TelefoneCel char(14);
+alter table Estabelecimento modify column CNPJ  (14);
+
+
+desc Representante;
+desc Logradouro;
+desc Estabelecimento;
+
+
+insert into Estabelecimento values
+(null,'56895289000109','Fazenda do carmo','FazendaCarmo@gmail.com',null),
+(null,'54837088000120','TorresSoja','roberto13torres@gmail.com',null),
+(null,'83155573000111','camposoja','LUCASMARTINS@gmail.com','23252928'),
+(null,'12365987458978','Ranch Magalhães','magalhaes1958@gmail.com','26699898'),
+(null,'18381725000132','mundosoja','mundosoja@gmail.com','32365698'),
+(null,'43324579000150','Rei da soja','reidasoja23@gmail.com',null);
+
+
+insert into Representante  values
+(null,'Gabriel','Silva','GabrielSilva','Gabriel1513','gabrielsilva@gmail.com','(15)99898-3698',1),
+(null,'Vanderlei','Matos','vandinho','Vander2689','vanderleimatos@gmail.com','(16)95686-3788',5),
+(null,'Roberto','Torres','Torres4588','6968torres','roberto13torres@gmail.com','(65)98986-4588',2),
+(null,'Marcia','Magalhães','Marcia1958','marcia7878','magalhaes1958@gmail.com','(66)98785-4589',4),
+(null,'Matheus','Martins','matheuzinho','matheus2004','matheuzinho@gmail.com','(66)98123-8987',6),
+(null,'Lucas','Martins','LMartins','lucasmartins0665','LUCASMARTINS@gmail.com','(19)97878-8987',3);
+
+select * from Estabelecimento;
+select * from Representante;
+
+
